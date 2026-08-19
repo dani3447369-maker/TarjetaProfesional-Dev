d96401d171c25105aad1d1afcf4f86101d0c4294
sapolin <sjdbcubdc8uyqbew8ucbqw8uybcw@ganiudlkm.com>
2026-08-19 09:33:12 -0500

    test animation

--- PasitoAPasito/src/app.js
+++ PasitoAPasito/src/app.js
@@ -1,7 +1,7 @@
 const onxrloaded = () => {
   XR8.XrController.configure({
     imageTargetData: [
-      require('../image-targets/TARJET_PROFESIONALl.json'),
+      require('../image-targets/TARJET_PROFESIONAL.jpg.json'),
     
     ],
   })
